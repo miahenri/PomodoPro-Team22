15b0641cea4329a5f07d94c06bfeefad951f676e
ricardotimmr <ricardo.timmr@gmail.com>
2024-06-17 13:36:15 +0200

    MainActivity is now the timer

--- app/src/main/java/com/example/pomodoro_22/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/pomodoro_22/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pomodoro_22.ui.timer
+package com.example.pomodoro_22.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.pomodoro_22.ui.theme.Pomodoro22Theme
+import com.example.pomodoro_22.ui.main.ui.theme.Pomodoro22Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
