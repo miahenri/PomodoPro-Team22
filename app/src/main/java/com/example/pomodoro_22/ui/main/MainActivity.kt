bf6ae7553d0bd1bc845260f8765df12594ce1d2a
ricardotimmr <ricardo.timmr@gmail.com>
2024-06-18 16:52:05 +0200

    Added taskbar in mainactivity

--- app/src/main/java/com/example/pomodoro_22/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/pomodoro_22/ui/main/MainActivity.kt
@@ -4,27 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pomodoro_22.R
 import com.example.pomodoro_22.ui.main.ui.theme.Pomodoro22Theme
 import com.example.pomodoro_22.ui.main.ui.theme.darkMode
-import com.example.pomodoro_22.ui.main.ui.theme.thinLine
 import com.example.pomodoro_22.ui.main.ui.theme.white
 
 class MainActivity : ComponentActivity() {
@@ -36,60 +29,99 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = darkMode // Grey color
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(top = 50.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        pomodoro("Pomodoro")
-                        Spacer(modifier = Modifier.height(15.dp))
-                        Box(
-                            modifier = Modifier
-                                .width(350.dp) // width of the line
-                                .height(1.dp) // height of the line
-                                .background(thinLine)
-                        )
-                        Spacer(modifier = Modifier.weight(1f)) // Spacer to push content to top
-                    }
-                }
+                ) { MainScreen() }
             }
         }
     }
 }
 
 @Composable
-fun pomodoro(name: String, modifier: Modifier = Modifier) {
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 10.dp)
+            .padding(top = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RoundedIconButton(
+                onClick = { /* TODO: Implement action */ },
+                icon = R.drawable.addtaskicon,
+                contentDescription = "Add Task"
+            )
+
+            PomodoroTitle(name = "Pomodoro")
+
+            RoundedIconButton(
+                onClick = { /* TODO: Implement action */ },
+                icon = R.drawable.settingsicon,
+                contentDescription = "Settings"
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        DividerLine()
+        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+fun PomodoroTitle(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "$name",
+        text = name,
         color = white, // Set text color to white
-        fontSize = 32.sp, // Increase text size
-        fontWeight = FontWeight.Bold, // Make text bold
-        textAlign = TextAlign.Center, // Center the text
+        fontSize = 38.sp, // Increase text size
+        fontWeight = FontWeight.Normal, // Make text bold
         modifier = modifier
+    )
+}
+
+@Composable
+fun DividerLine() {
+    Box(
+        modifier = Modifier
+            .width(350.dp) // width of the line
+            .height(1.dp) // height of the line
+            .background(Color.Gray)
+    )
+}
+
+@Composable
+fun RoundedIconButton(
+    onClick: () -> Unit,
+    icon: Int,
+    contentDescription: String
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier.size(56.dp),
+        content = {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = contentDescription,
+                tint = white
+            )
+        }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PomodoroTitlePreview() {
     Pomodoro22Theme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 50.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            pomodoro("Pomodoro Timer")
-            Spacer(modifier = Modifier.height(8.dp))
-            Box(
-                modifier = Modifier
-                    .width(150.dp) // width of the line
-                    .height(1.dp) // height of the line
-                    .background(white)
-            )
-            Spacer(modifier = Modifier.weight(1f)) // Spacer to push content to top
-        }
+        PomodoroTitle(name = "Pomodoro Timer")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DividerLinePreview() {
+    Pomodoro22Theme {
+        DividerLine()
     }
 }
